af41fe5310040dd76a14a99dcb70fbd52afa1154
DAM119 <DAM119@C0121.iesataulfo.local>
2023-03-22 17:17:14 +0100

    Codigo mensaje por pantalla creado para rama 1 del repositorio

---
+++ Proyecto_1/src/PaqueteRama1/Rama1.java
@@ -0,0 +1,10 @@
+package PaqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Codigo mensaje por pantalla creado para rama 1 del repositorio");
+
+	}
+
+}
--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Programa que nos permitira hacer un control de versiones con git"
-				+ "Segunda modificacion");
+				+ "Segunda modificacion ");
 	}
 	
 
